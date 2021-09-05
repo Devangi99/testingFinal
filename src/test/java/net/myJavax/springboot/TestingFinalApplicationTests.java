ece69fcf0324f05e93fe4aa430ec4691d9763cbf
Devangi99 <dgamage102@gmail.com>
2021-09-05 21:09:43 +0530

    first commit

---
+++ src/test/java/net/myJavax/springboot/TestingFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package net.myJavax.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
